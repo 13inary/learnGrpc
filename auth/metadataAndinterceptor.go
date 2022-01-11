07c452abb7c965bf9ceae91b9e2132c137ff98ad
keysquivered <936095897@qq.com>
2022-01-11 17:21:10 +0800

    add interceptor and metadata

---
+++ auth/metadataAndinterceptor.go
@@ -0,0 +1,5 @@
+package auth
+
+// package auth
+
+//
---
+++ metadata/getMetadata.go
@@ -0,0 +1,18 @@
+package metadata
+
+// package metadata
+
+//type server struct {
+//}
+//
+//func (s *server) SomeRPC(ctx context.Context, in *pd.SomeReq) (*pd.SomeRsp, error) {
+//	md, ok := metadata.FromIncomingContext(ctx)
+//	if !ok {
+//		// aaaa
+//		log.Println("get metadata failure")
+//	}
+//	fmt.Printf("md = %+v\n", md)
+//	log.Println("do you want by metadata")
+//
+//	return nil, nil
+//}
